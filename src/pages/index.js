2b37f5942026009bcd3395376b1b02c395d7aa7c
Samwel Charles <younggeeks101@gmail.com>
2019-08-15 12:58:50 +0300

    Remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
 import React, { Component } from "react"
-import * as d3 from "d3"
-import _ from "lodash"
-import Graph from "../components/graph"
-import BarChart from "../components/Gra"
 import BSTree from "../components/Tree"
 class IndexPage extends Component {
   render() {
