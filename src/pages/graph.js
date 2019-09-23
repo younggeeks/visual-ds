b7ccfe051cad7eccbdf857501ee049e4015c5a68
Samwel Charles <younggeeks101@gmail.com>
2019-09-23 22:30:42 +0300

    Add click and reformat Nodes UI Add Graph data structure implementation

---
+++ src/pages/graph.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import GraphDS from "../components/GraphDS"
+
+const SecondPage = () => (
+   <GraphDS/>
+)
+
+export default SecondPage
